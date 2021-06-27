730f192f5c745ff2660609cc0b56be4dffea29d3
Aleksander Wojcik <aleksander.w1992@gmail.com>
2021-06-27 23:23:31 +0200

    Initialize git repository

---
+++ src/main/java/com/alex/wojcik/reportweb/ReportWebApplication.java
@@ -0,0 +1,13 @@
+package com.alex.wojcik.reportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/wojcik/reportweb/ReportWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.wojcik.reportweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
